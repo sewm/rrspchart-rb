a8709b2bc4709122c5bd9e9b817728dd7b6729bb
Sam MacCutchan <sam@sewm.org>
2013-02-24 16:55:54 -0500

    Create arrays of prices, units and values. Calculate and display maximums of each.

--- chart.rb
+++ chart.rb
@@ -12,17 +12,27 @@ data = CSV.read("./data.csv")
 
 first_date = Time.now
 last_date = Time.mktime(1900,1,1)
+prices = Array.new(0)
+units = Array.new(0)
+values = Array.new(0)
 
 data.each do |d|
   current_date = d[0].split("-")
   current_date = Time.mktime(current_date[0],current_date[1],current_date[2])
   first_date = current_date if current_date < first_date
   last_date = current_date if current_date > last_date
+  
+  prices.push(d[1])
+  units.push(d[2])
+  values.push(d[3])
 end
 
 get '/' do
   @first_date = first_date
   @last_date = last_date
+  @max_price = prices.max
+  @max_units = units.max
+  @max_value = values.max
   
   haml :index, :format => :html5
 end
