ba85eb556c04bf1ad00561aaa1ddcb258acf3491
Sam MacCutchan <sam@sewm.org>
2013-02-24 16:27:31 -0500

    Read in the data file and determine the earliest and latest dates.

--- chart.rb
+++ chart.rb
@@ -8,7 +8,22 @@ require 'sinatra'
 require 'haml'
 require 'CSV'
 
+data = CSV.read("./data.csv")
+
+first_date = Time.now
+last_date = Time.mktime(1900,1,1)
+
+data.each do |d|
+  current_date = d[0].split("-")
+  current_date = Time.mktime(current_date[0],current_date[1],current_date[2])
+  first_date = current_date if current_date < first_date
+  last_date = current_date if current_date > last_date
+end
+
 get '/' do
+  @first_date = first_date
+  @last_date = last_date
+  
   haml :index, :format => :html5
 end
 
