b0bf4e923595a53cedadfde2454d42ffaadc8d46
Sam MacCutchan <sam@sewm.org>
2013-02-27 14:45:17 -0500

    Add default values for date and units field.

--- chart.rb
+++ chart.rb
@@ -37,6 +37,22 @@ get '/' do
   @max_units = units.max
   @max_value = values.max
   
+  # For populating the form with default values for units.
+  @current_units = units[units.length - 1]
+  
+  # And for the date field. We are almost always entering the previous day's date,
+  # or the previous Friday if the current date is Saturday, Sunday or Monday.
+  # So let's populate the form with that day to save time.
+  @current_time = Time.now
+  
+  if @current_time.monday?
+    @current_time = @current_time - (1440 * 60 * 3) # Subtract three days
+  elsif @current_time.sunday?
+    @current_time = @current_time - (1440 * 60 * 2) # Subtract two days
+  else
+    @current_time = @current_time - (1440 * 60) # Subtract one day
+  end
+  
   haml :index, :format => :html5
 end
 
